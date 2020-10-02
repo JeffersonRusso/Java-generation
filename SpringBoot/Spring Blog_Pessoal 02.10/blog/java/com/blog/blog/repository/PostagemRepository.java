a63d4b68e0346c30797c23ec652bcab79076b2f1
unknown <jefferson.russo@Outlook.com>
2020-10-02 16:07:54 -0300

    02.10

---
+++ SpringBoot/Spring Blog_Pessoal 02.10/blog/java/com/blog/blog/repository/PostagemRepository.java
@@ -0,0 +1,18 @@
+package com.blog.blog.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blog.blog.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase ( String t);
+	
+	
+	
+	
+}
